bd3fca302a15f0901cbcac06c9d0e80111055fad
Christopher Jones <tkcwebdev@gmail.com>
2020-03-22 02:56:25 -0700

    Burger is ready to start accepting ingredients

--- reactTraining/src/components/assignmentThree/burger/Burger.js
+++ reactTraining/src/components/assignmentThree/burger/Burger.js
@@ -3,18 +3,34 @@ import './Burger.scss';
 
 import BurgerIngredient from '../burgerIngredient/BurgerIngredient';
 
-export default function Burger() {
+export default function Burger(props) {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+        return [...Array(props.ingredients[ingKey])].map((_, i) => {
+           return <BurgerIngredient key={ingKey + i} type={ingKey} />
+        });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    console.log(transformedIngredients.length);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients!</p>
+    }
+    
     return (
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="bacon"/>
+            {transformedIngredients}
+            {/* <BurgerIngredient type="bacon"/>
             <BurgerIngredient type="lettuce"/>
             <BurgerIngredient type="cheese"/>
             <BurgerIngredient type="meat"/>
             <BurgerIngredient type="lettuce"/>
             <BurgerIngredient type="cheese"/>
             <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="lettuce"/>
+            <BurgerIngredient type="lettuce"/> */}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
--- reactTraining/src/components/assignmentThree/containers/burgerBuilder/BurgerBuilder.js
+++ reactTraining/src/components/assignmentThree/containers/burgerBuilder/BurgerBuilder.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BurgerBuilder.scss';
 
 import Burger from '../../burger/Burger';
 
 export default function BurgerBuilder() {
+    const [ingredients, setIngredients] = useState({
+            // lettuce: 1,
+            // bacon: 1,
+            // cheese: 2,
+            // meat: 2,
+            // price: "39.99"
+            lettuce: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+            price: 0
+    })
     return (
         <div className="BurgerBuilder">
-            <Burger />
+            <Burger ingredients={ingredients}/>
             <div className="BurgerControls">Build Controls</div>
         </div>
     )
