30c60231f4cf7632a1a1eb10c07993e6d8faa292
tkcwebdev <tkcwebdev@gmail.com>
2020-04-09 12:03:35 -0700

    Fixed Modal overscroll issue, Added Print Order details and order detail modal view from Orders page

--- reactTraining/src/components/assignmentThree/containers/orders/Orders.js
+++ reactTraining/src/components/assignmentThree/containers/orders/Orders.js
@@ -1,54 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import './Orders.scss';
-import db from '../../firebase/firebaseConfig';
-import Order from '../../order/Order';
-import Spinner from '../../UI/spinner/Spinner';
+import React, { useEffect, useState } from "react";
+import "./Orders.scss";
+import db from "../../firebase/firebaseConfig";
+import Order from "../../order/Order";
+import Spinner from "../../UI/spinner/Spinner";
 
 export default function Orders(props) {
-    const [orders,setOrders] = useState([]);
-    const [loading,setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const getOrders = () => {
-        db.collection('orders').get().then(snapshot => {
-            snapshot.forEach(doc => {
-                let order = doc.data();
-                order.id = doc.id;
-                setOrders(orders => orders.concat(order));
-                setLoading(false);
-                // console.log(doc.data());
-            })
-        })
-    }
+  const getOrders = () => {
+    db.collection("orders")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          let order = doc.data();
+          order.id = doc.id;
+          setOrders(orders => orders.concat(order));
+          setLoading(false);
+          // console.log(doc.data());
+        });
+      });
+  };
 
-    useEffect(() => {
-        getOrders();
-    }, [])
+  useEffect(() => {
+    getOrders();
+  }, []);
 
+  let orderList = orders.map(order => {
+    return <Order data={order} key={order.id} />;
+  });
 
-let orderList = (
-    orders.map(order => {
-        return <Order data={order} key={order.id} />
-    })
-)
-
-    return (
-        <div className='Orders'>
-            <h1>Orders</h1>
-            <ul className="order-data-table">
-                <li>
-                    <tr>
-                        <td className="order-date strong">Order Date</td>
-                        <td className="order-id strong">Order ID</td>
-                        <td className="order-name strong">Name</td>
-                        <td className="order-email strong">Email</td>
-                        <td className="order-address strong">Address</td>
-                        <td className="order-zip strong">Zip Code</td>
-                        <td className="order-price strong">Total Price</td>
-                        <td className="order-delivery strong">Delivery Method</td>
-                    </tr>
-                </li>
-                {loading ? <Spinner /> : orderList}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="Orders">
+      <h1>Orders</h1>
+      <ul className="order-data-table">
+        <li>
+          <table>
+            <tbody>
+              <tr>
+                <td className="order-date strong">Order Date</td>
+                <td className="order-id strong">Order ID</td>
+                <td className="order-name strong">Name</td>
+                <td className="order-email strong">Email</td>
+                <td className="order-address strong">Address</td>
+                <td className="order-zip strong">Zip Code</td>
+                <td className="order-price strong">Total Price</td>
+                <td className="order-delivery strong">Delivery Method</td>
+              </tr>
+            </tbody>
+          </table>
+        </li>
+        {loading ? <Spinner /> : orderList}
+      </ul>
+    </div>
+  );
 }
--- reactTraining/src/components/assignmentThree/order/Order.js
+++ reactTraining/src/components/assignmentThree/order/Order.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import './Order.scss'
+import React, { Fragment, useState } from 'react';
+import './Order.scss';
+import Modal from '../UI/modal/Modal';
+import Logo from '../assets/logo/Logo';
 
 export default function Order(props) {
+    const [orderClicked,setOrderClicked] = useState(false);
     let order = props.data;
+
+    const toggleModal = () => {
+        console.log('Toggled')
+        setOrderClicked(!orderClicked);
+    }
+
+    const ingredients = [];
+
+    for (let ing in order.ingredients) {
+        ingredients.push({
+            name: ing,
+            amount: order.ingredients[ing],
+        })
+    }
 
     const convertDate = (timestamp) => {
         let date = timestamp.toDate();
@@ -14,17 +31,49 @@ export default function Order(props) {
       }
 
     return (
-            <li className="Order">
-                    <tr>
-                        <td className="order-date">{convertDate(order.dateAdded)}</td>
-                        <td className="order-id">{order.id}</td>
-                        <td className="order-name">{order.customerData.name}</td>
-                        <td className="order-email">{order.customerData.email}</td>
-                        <td className="order-address">{order.customerData.address.street}</td>
-                        <td className="order-zip">{order.customerData.address.zipCode}</td>
-                        <td className="order-price"><sup>$</sup>{order.totalPrice.toFixed(2)}</td>
-                        <td className="order-delivery">{order.deliveryMethod}</td>
-                    </tr>
+        <Fragment>
+            { orderClicked &&
+                <Modal close={toggleModal}>
+                    <div className='order-details'>
+                        <h1><Logo/> <span>Order Details</span></h1>
+                        <hr/>
+                        <h3>{order.id}<span>{convertDate(order.dateAdded)}</span><br/></h3>
+                        <hr/>
+                        <h4>Customer Details</h4>
+                        <p>Name: <span></span><span><strong>{order.customerData.name}</strong></span><br/>
+                        Email: <span><strong>{order.customerData.email}</strong></span><br/>
+                        Street Address: <span><strong>{order.customerData.address.street}</strong></span><br/>
+                        Zip Code: <span><strong>{order.customerData.address.zipCode}</strong></span></p>
+                        <hr/>
+                        <h4>Order Details</h4>
+                        <ul>
+                            {ingredients.map(ig => {
+                            return <li key={ig.name}>{ig.name}: <span><strong>{ig.amount}</strong></span></li>
+                            })}
+                        </ul>
+                        <p>Total Price <span><strong><sup>$</sup>{order.totalPrice.toFixed(2)}</strong></span></p>
+                        <hr/>
+                        <button className="btn prime" onClick={(e) => { window.print('.order-details') }} >Print This Page</button>
+                        <button className='btn link' onClick={toggleModal}>Close</button>
+                    </div>
+                </Modal>
+            }
+            <li className="Order" onClick={toggleModal}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="order-date">{convertDate(order.dateAdded)}</td>
+                            <td className="order-id">{order.id}</td>
+                            <td className="order-name">{order.customerData.name}</td>
+                            <td className="order-email">{order.customerData.email}</td>
+                            <td className="order-address">{order.customerData.address.street}</td>
+                            <td className="order-zip">{order.customerData.address.zipCode}</td>
+                            <td className="order-price"><sup>$</sup>{order.totalPrice.toFixed(2)}</td>
+                            <td className="order-delivery">{order.deliveryMethod}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </li>
+        </Fragment>
     )
 }
